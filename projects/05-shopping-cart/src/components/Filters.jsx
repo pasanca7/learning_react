fb9199617717247d626dd6c6354f13e0d471ca4f
Pablo de los Santos Carrión <pablodelossantoscarrion@gmail.com>
2024-02-26 23:14:51 +0100

    New project shopping card

---
+++ projects/05-shopping-cart/src/components/Filters.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './Filters.css'
+
+export function Filters() {
+    const [minPrice, setMinPrice] = useState(0)
+
+    const handleChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+    }
+
+    return(
+        <section className="filters">
+            <div>
+                <label htmlFor="price">Min Price</label>
+                <input 
+                    type="range"
+                    id="price"
+                    min='0'
+                    max='1000'
+                    onChange={handleChangeMinPrice}
+                />
+                <span>${minPrice}</span>
+            </div>
+
+            <div>
+                <label htmlFor="category">Category</label>
+                <select id="category">
+                    <option value="all">All</option>
+                    <option value="laptops">Laptops</option>
+                    <option value="smartphones">Smartphones</option>
+                </select>
+            </div>
+        </section>
+    )
+}
